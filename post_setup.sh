131df661740beeed5e70848607a4bd98fad60521
Jens Bodal <jensbodal@gmail.com>
2020-07-19 16:19:32 -0700

    add additional setup, remove vimproc and submodule install

--- post_setup.sh
+++ post_setup.sh
@@ -1,5 +1,11 @@
-git submodule update --init
-(cd .vim/bundle/vimproc.vim; make)
+#(cd .vim/bundle/vimproc.vim; make)
+
+if ! type go>/dev/null; then
+  echo "Install go"
+  echo "brew install go"
+  echo "brew install coreutils"
+  exit 1
+fi
 
 if ! type direnv>/dev/null; then
   echo "Install direnv: https://github.com/direnv/direnv/blob/master/docs/development.md"
@@ -9,7 +15,9 @@ if ! type direnv>/dev/null; then
   echo "cd direnv"
   echo "make"
   echo "make install"
+  exit 1
 fi
+
 
 if type npm>/dev/null; then
   npm config set prefix=$HOME/local/npm
@@ -20,19 +28,23 @@ if type npm>/dev/null; then
     yarn global add jwt-cli
   else
     echo "Install yarn: https://classic.yarnpkg.com/en/docs/install/#manual-install-via-tarball"
+    exit 1
   fi
 else
   echo "Install node: https://nodejs.org/en/download/"
-  echo "  1. install node (tar vxf *.tar.xz)"
+  echo "  1. install node to $HOME/node-temp (tar vxf *.tar.xz)"
   echo "  2. symlink node and npm versions to ~/local/bin"
-  echo "     - cd /home/bodal/download/node-v12.16.3-linux-x64/bin"
+  echo "     - cd node*/bin"
   echo "     - ln -s \$(readlink -f node) \$HOME/local/bin/node"
   echo "     - ln -s \$(readlink -f ../lib/node_modules/npm/bin/npm-cli.js) \$HOME/local/bin/npm"
   echo "  3. install yarn (tar zvxf): https://classic.yarnpkg.com/en/docs/install/#manual-install-via-tarball"
+  echo "     - brew install yarn"
+  echo "     or"
   echo "     - ln -s $(readlink -f yarn) $HOME/local/bin/yarn"
   echo "  4. re-run this script"
   echo "  -. install n"
   echo "  -. use n to install node/npm"
   echo "  -. remove symlinks and manual download of node"
+  exit 1
 fi
 
