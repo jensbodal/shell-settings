e856dce9ff7498df1b9a7649617d72a23f3af72a
Jens Bodal <jensbodal@gmail.com>
2024-09-17 17:56:00 -0700

    add script init and run symlink setup when running update script

---
+++ .vim/templates/script-init.sh
@@ -0,0 +1,1 @@
+[ ! "$_INIT_COMPLETE" ] && source $HOME/github/shell-settings/scripts/.INIT && export _INIT_COMPLETE=1
--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -10,11 +10,10 @@ createsymlink() {
   if [ ! -L "$d" ]; then
     echo "! linking \"$s\" to \"$d\""
     ln -sf "$s" "$d"
-  else
-    echo "symlink \"$d\" already exists"
   fi
 }
 
+echo "Checking for symlink update..."
 createsymlink $THIS_FOLDER/.zshrc $HOME/.zshrc
 createsymlink $THIS_FOLDER/.aliasrc-shell-settings $HOME/.aliasrc-shell-settings
 createsymlink $THIS_FOLDER/.zsh_completions $HOME/.zsh_completions
@@ -23,10 +22,18 @@ createsymlink $THIS_FOLDER/.direnvrc $HOME/.direnvrc
 
 createsymlink $THIS_FOLDER/jens-disagrees.zsh-theme $HOME/.oh-my-zsh/themes/jens-disagrees.zsh-theme
 
-createsymlink $THIS_VIM_FOLDER/after $HOME/.vim/after
-createsymlink $THIS_VIM_FOLDER/autoload $HOME/.vim/autoload
-createsymlink $THIS_VIM_FOLDER/bundle $HOME/.vim/bundle
-createsymlink $THIS_VIM_FOLDER/colors $HOME/.vim/colors
-createsymlink $THIS_VIM_FOLDER/ftplugin $HOME/.vim/ftplugin
-createsymlink $THIS_VIM_FOLDER/plugin $HOME/.vim/plugin
+for d in `find ${THIS_VIM_FOLDER} -type d`; do
+  vimfolder=`basename ${d}`
+  symlink="${HOME}/.vim/${vimfolder}"
 
+  if [ ! -h "$symlink" ]; then
+    createsymlink "${d}" "${symlink}"
+  fi
+done
+#createsymlink $THIS_VIM_FOLDER/after $HOME/.vim/after
+#createsymlink $THIS_VIM_FOLDER/autoload $HOME/.vim/autoload
+#createsymlink $THIS_VIM_FOLDER/bundle $HOME/.vim/bundle
+#createsymlink $THIS_VIM_FOLDER/colors $HOME/.vim/colors
+#createsymlink $THIS_VIM_FOLDER/ftplugin $HOME/.vim/ftplugin
+#createsymlink $THIS_VIM_FOLDER/plugin $HOME/.vim/plugin
+#createsymlink $THIS_VIM_FOLDER/templates $HOME/.vim/templates
